76d49eca89300664b639038e7fe42122d3499e7d
Adithya Sapalya <adithyasapalya30@gmail.com>
2025-12-29 22:02:53 +0530

    number of racers acquired

--- tr.py
+++ tr.py
@@ -1,4 +1,16 @@
 def get_number_of_racers():
     racers = 0
     while True:
-        
+        racers = input ("Enter the number of racers (2-10): ")
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print("Input is not numeric... Try again!!.")
+            continue
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print("Number of racers must be between 2 and 10... Try again!!.")
+
+racers = get_number_of_racers()
+print(racers)
